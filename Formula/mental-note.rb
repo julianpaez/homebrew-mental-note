c46669c03d4c78eb13c6d11be9b85c039bc0ff79
Julian Paez <paezjulian@gmail.com>
2025-11-15 20:15:17 -0500

    fix help

--- Formula/mental-note.rb
+++ Formula/mental-note.rb
@@ -10,7 +10,7 @@ class MentalNote < Formula
   # When hosting in GitHub, this will be:
   # https://github.com/YOUR_USERNAME/homebrew-mental-note/raw/main/releases/mental-note-v1.5.0-darwin-arm64.tar.gz
   url "https://github.com/julianpaez/homebrew-mental-note/raw/main/releases/mental-note-v1.5.0-darwin-arm64.tar.gz"
-  sha256 "d187fbfc10608a2eca9fc5990b3e49d44c96a64612a1896e135e68eb2c6f57fe"
+  sha256 "7ef517887f437c605090d9611b704d206af72106c389f70407ffa6141db478df"
 
   # System requirements
   depends_on :macos
