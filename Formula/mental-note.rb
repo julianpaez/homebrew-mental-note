8e4182b8fef30982324fed712ad94d2e3da2bb00
Julian Paez <paezjulian@gmail.com>
2025-11-23 08:15:35 -0500

    New version

--- Formula/mental-note.rb
+++ Formula/mental-note.rb
@@ -4,13 +4,13 @@ class MentalNote < Formula
   desc "Zero-dependency CLI for managing mental notes, tasks, and ideas with gamification"
   homepage "https://github.com/julianpaez/cli-mental-note"
   license "MIT"
-  version "1.8.0"
+  version "1.9.0"
 
   # Download URL - Points to tar.gz in this repository's releases/ folder
   # When hosting in GitHub, this will be:
-  # https://github.com/YOUR_USERNAME/homebrew-mental-note/raw/main/releases/mental-note-v1.8.0-darwin-arm64.tar.gz
-  url "https://github.com/julianpaez/homebrew-mental-note/raw/main/releases/mental-note-v1.8.0-darwin-arm64.tar.gz"
-  sha256 "7c23955c143494411bba77f11bd7757b16bb793abae972ce6480805e19cbe7bd"
+  # https://github.com/YOUR_USERNAME/homebrew-mental-note/raw/main/releases/mental-note-v1.9.0-darwin-arm64.tar.gz
+  url "https://github.com/julianpaez/homebrew-mental-note/raw/main/releases/mental-note-v1.9.0-darwin-arm64.tar.gz"
+  sha256 "bede0b81446fb68849af8cb211bf32ee8569314b82ce1afe57984672ae151ac3"
 
   # System requirements
   depends_on :macos
@@ -30,7 +30,7 @@ class MentalNote < Formula
 
   def caveats
     <<~EOS
-      Mental Note CLI v1.8.0 has been installed!
+      Mental Note CLI v1.9.0 has been installed!
 
       You can use either command:
         mental-note --help
@@ -44,8 +44,8 @@ class MentalNote < Formula
         mn badges           # See achievements
         mn version
 
-      New in v1.8.0:
-        • Natural Language Input and Goals
+      New in v1.9.0:
+        • Short names for commands
 
       For more information:
         https://github.com/julianpaez/cli-mental-note
@@ -54,8 +54,8 @@ class MentalNote < Formula
 
   test do
     # Test that the binary runs and shows version
-    assert_match "1.8.0", shell_output("#{bin}/mental-note --version")
-    assert_match "1.8.0", shell_output("#{bin}/mn --version")
+    assert_match "1.9.0", shell_output("#{bin}/mental-note --version")
+    assert_match "1.9.0", shell_output("#{bin}/mn --version")
 
     # Test basic functionality
     system "#{bin}/mental-note", "version"
