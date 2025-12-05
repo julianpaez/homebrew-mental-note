61d60de5940a662764ff9b40cb4f2ed9b46accd4
Julian Paez <paezjulian@gmail.com>
2025-12-05 15:25:20 -0500

    Release mental-note v2.0.5 for macOS ARM64
    Update formula, documentation, and release artifacts to version 2.0.5. This includes updating the Homebrew formula with the new version, checksum, and test assertions, as well as adding the new binary and checksum files.

--- Formula/mental-note.rb
+++ Formula/mental-note.rb
@@ -10,7 +10,7 @@ class MentalNote < Formula
   # When hosting in GitHub, this will be:
   # https://github.com/YOUR_USERNAME/homebrew-mental-note/raw/main/releases/mental-note-v2.0.4-darwin-arm64.tar.gz
   url "https://github.com/julianpaez/homebrew-mental-note/raw/main/releases/mental-note-v2.0.4-darwin-arm64.tar.gz"
-  sha256 "e8c2a73f5c09da5d7963bab5623bcc37349e96c226dd8c0dd29a9e2eef79d0ca"
+  sha256 "68eb1449f674ee8e6f4c8ef26a4354d38f1b8feff0f556aa022dae4e919049b8"
 
   # System requirements
   depends_on :macos
@@ -30,7 +30,7 @@ class MentalNote < Formula
 
   def caveats
     <<~EOS
-      Mental Note CLI v2.0.4 has been installed!
+      Mental Note CLI v2.0.5 has been installed!
 
       You can use either command:
         mental-note --help
@@ -44,7 +44,7 @@ class MentalNote < Formula
         mn badges           # See achievements
         mn version
 
-      New in v2.0.4:
+      New in v2.0.5:
         • fix: date format
 
       For more information:
@@ -54,8 +54,8 @@ class MentalNote < Formula
 
   test do
     # Test that the binary runs and shows version
-    assert_match "2.0.4", shell_output("#{bin}/mental-note --version")
-    assert_match "2.0.4", shell_output("#{bin}/mn --version")
+    assert_match "2.0.5", shell_output("#{bin}/mental-note --version")
+    assert_match "2.0.5", shell_output("#{bin}/mn --version")
 
     # Test basic functionality
     system "#{bin}/mental-note", "version"
